1b12efb2e63b74d25109fccc75d4aebbc0da268d
wade12 <wade@10.0.0.135>
2013-06-13 17:54:59 +0100

    company logo

--- gen/com/skynet/support/R.java
+++ gen/com/skynet/support/R.java
@@ -14,7 +14,7 @@ public final class R {
         public static final int comments_height=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int skynetlabs=0x7f020000;
     }
     public static final class id {
         public static final int btnSendSupport=0x7f090000;
